529ca174b0bc07927c60c4e80bcf2a993d01fd83
Varun Chitturi <varunchitturi@icloud.com>
2021-01-31 18:40:15 -0800

    add login and logout service

--- public/auth.js
+++ public/auth.js
@@ -4,7 +4,7 @@
 
 const auth = firebase.auth()
 
-function signup(){
+function authenticate(){
     const pass = document.getElementById("pass").value
     const repass = document.getElementById("re_pass").value
     const email = document.getElementById("email").value
@@ -14,6 +14,7 @@ function signup(){
         if(pass === repass && pass.length >= 8){
             auth.createUserWithEmailAndPassword(email,pass).then(cred => {
                 document.getElementById("signUpMessage").innerHTML = "You have created an account"
+                window.location.href = "dashboard.html";
             }).catch(err => document.getElementById("signUpMessage").innerHTML = err.message)
         }
         else if(pass.length < 8){
@@ -26,4 +27,22 @@ function signup(){
     catch(err){
         document.getElementById("signUpMessage").innerHTML = err.message;
     }
+}
+
+function logout(){
+    auth.signOut().then(r => {
+        console.log("user has signed out")
+        window.location.href = "index.html";
+    }).catch(err => {console.log(err.message)});
+}
+function login(){
+    const email = document.getElementById("login-email").value
+    const pass = document.getElementById("login-pass").value
+    auth.signInWithEmailAndPassword(email,pass).then(cred => {
+        // TODO: remove this console log
+        console.log(cred);
+        window.location.href = "dashboard.html";
+
+    })
+
 }
