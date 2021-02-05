6997b4538aecb168227543522f7e1a09ef467df7
Varun Chitturi <varunchitturi@icloud.com>
2021-02-04 22:45:04 -0800

    Add forgot password option

---
+++ public/forgotpass.js
@@ -0,0 +1,14 @@
+const auth = firebase.auth()
+document.getElementById("resend-description").hidden = true
+
+
+function sendReset(){
+    auth.sendPasswordResetEmail(document.getElementById("user-email").value).then(function() {
+        document.getElementById("resend-description").hidden = false
+        document.getElementById("resend-description").value = "If there is an account under this email, a reset link has been sent"
+    }).catch(function(error) {
+        document.getElementById("resend-description").hidden = false
+        console.log(error.message)
+        document.getElementById("resend-description").value = "If there is an account under this email, a reset link has been sent"
+    });
+}
