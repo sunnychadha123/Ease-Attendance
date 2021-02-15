fedc9d8d3dc75b5d4cd93af48eb82c7a33ddd364
Varun Chitturi <varunchitturi@icloud.com>
2021-02-14 20:20:02 -0800

    log req.query for testing

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.get('/verify', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/verify.html'));
 })
 app.get('/authorize', (req, res) => {
-    console.log(req.params.code)
+    console.log(req.query.code)
     res.sendFile(path.join(__dirname + '/public/signup.html'));
 })
 app.get('/zoomverify/verifyzoom.html', (req, res) => {
