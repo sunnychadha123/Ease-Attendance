a02dba3ba715f75d48049ce707f1fc0b7456a4bb
adityapatel-sudo <adityapatel1204@gmail.com>
2021-03-01 02:59:34 -0800

    Added photoes and reforming documentation.html

--- Tests/meetingEvents.py
+++ Tests/meetingEvents.py
@@ -4,11 +4,11 @@
 import requests
 from time import sleep
 import names
-url = "https://www.easeattendance.com/api/requests"
-# url = "http://localhost:4000/api/requests"
+
 
 def startMeeting(meetingName, meetingID, hostID):
-
+    url = "https://www.easeattendance.com/api/requests"
+    url = "http://localhost:4000/api/requests"
 
     payload = "{\r\n  \"event\": \"meeting.started\",\r\n  \"event_ts\": 1234566789900,\r\n  \"payload\": {\r\n    \"account_id\": \"o8KK_AAACq6BBEyA70CA\",\r\n    \"object\": {\r\n      \"uuid\": \"czLF6FFFoQOKgAB99DlDb9g==\",\r\n      \"id\": \"" + meetingID + "\",\r\n      \"host_id\": \"" + hostID + "\",\r\n      \"topic\": \"" + meetingName + "\",\r\n      \"type\": 2,\r\n      \"start_time\": \"2019-07-09T17:00:00Z\",\r\n      \"duration\": 60,\r\n      \"timezone\": \"America/Los_Angeles\"\r\n    }\r\n  }\r\n}"
     headers = {
@@ -22,7 +22,8 @@ def startMeeting(meetingName, meetingID, hostID):
 
 
 def endMeeting(meetingName, meetingID, hostID):
-
+    url = "https://www.easeattendance.com/api/requests"
+    url = "http://localhost:4000/api/requests"
     payload = "{\n  \"event\": \"meeting.ended\",\n  \"event_ts\": 1234566789900,\n  \"payload\": {\n    \"account_id\": \"o8KK_AAACq6BBEyA70CA\",\n    \"object\": {\n      \"uuid\": \"czLF6FFFoQOKgAB99DlDb9g==\",\n      \"id\": \"" + meetingID + "\",\n      \"host_id\": \"" + hostID + "\",\n      \"topic\": \"" + meetingName + "\",\n      \"type\": 2,\n      \"start_time\": \"2019-07-09T17:00:00Z\",\n      \"duration\": 60,\n      \"timezone\": \"America/Los_Angeles\"\n    }\n  }\n}"
     headers = {
         'authorization': 'YYzbQmUuQ72ZZsbaionu2A',
@@ -35,7 +36,8 @@ def endMeeting(meetingName, meetingID, hostID):
 
 
 def addParticipant(participantName, meetingName, meetingID, hostID, email):
-
+    url = "https://www.easeattendance.com/api/requests"
+    url = "http://localhost:4000/api/requests"
     payload = "{\n  \"event\": \"meeting.participant_joined\",\n  \"event_ts\": \"long\",\n  \"payload\": {\n    \"account_id\": \"string\",\n    \"object\": {\n      \"id\": \"" + meetingID + "\",\n      \"uuid\": \"czLF6FFFoQOKgAB99DlDb9g==\",\n      \"host_id\": \"" + hostID + "\",\n      \"topic\": \"" + meetingName + "\",\n      \"type\": \"2\",\n      \"start_time\": \"string [date-time]\",\n      \"timezone\": \"string\",\n      \"duration\": \"integer\",\n      \"participant\": {\n        \"user_id\": \"11111111\",\n        \"user_name\": \"" + participantName + "\",\n        \"email\": \""+email+"\",\n        \"id\": \"string\",\n        \"join_time\": \"string [date-time]\"\n      }\n    }\n  }\n}"
     headers = {
         'authorization': 'YYzbQmUuQ72ZZsbaionu2A',
@@ -48,7 +50,8 @@ def addParticipant(participantName, meetingName, meetingID, hostID, email):
 
 
 def removeParticipant(participantName, meetingName, meetingID, hostID,email):
-
+    url = "https://www.easeattendance.com/api/requests"
+    url = "http://localhost:4000/api/requests"
     payload = "{\n    \"event\": \"meeting.participant_left\",\n    \"event_ts\": \"long\",\n    \"payload\": {\n        \"account_id\": \"string\",\n        \"object\": {\n            \"id\": \"" + meetingID + "\",\n            \"uuid\": \"czLF6FFFoQOKgAB99DlDb9g==\",\n            \"host_id\": \"" + hostID + "\",\n            \"topic\": \"" + meetingName + "\",\n            \"type\": \"2\",\n            \"start_time\": \"string [date-time]\",\n            \"timezone\": \"string\",\n            \"duration\": \"integer\",\n            \"participant\": {\n                \"user_id\": \"11111111\",\n                \"user_name\": \"" + participantName + "\",\n                \"email\": \""+email+"\",\n                \"id\": \"string\",\n                \"join_time\": \"string [date-time]\"\n            }\n        }\n    }\n}"
     headers = {
         'authorization': 'YYzbQmUuQ72ZZsbaionu2A',
@@ -63,39 +66,16 @@ def removeParticipant(participantName, meetingName, meetingID, hostID,email):
 VarunHostID = "hIk5FOWfR-SFE9DgN-2N2w"
 AdityaHostID = "TbQ_nGjpR9aMPQnS-IkQPQ"
 meetingID = "12345612390"
-meetingName = "TEST MEETING"
-
-participants = []
-
-
-input("press enter to start a meeting: ")
+meetingName = "a new meetinga sdf"
 
 startMeeting(meetingName, meetingID, AdityaHostID)
 
+input("next")
 
-participantCount = input("enter the number of participants you want to add: ")
-try:
-    participantCount = int(participantCount)
-except ValueError:
-    participantCount = 30
-    print("Defaulting to 30 participants")
-
-for x in range(participantCount):
-    firstName = names.get_first_name()
-    lastName = names.get_last_name()
-    addParticipant(firstName + " " + lastName, meetingName, meetingID, AdityaHostID,"generic@Email")
-    participants.append([firstName,lastName])
+for x in range(100):
+    addParticipant(names.get_first_name() + " " + names.get_last_name(), meetingName, meetingID, AdityaHostID,"generic Email")
     sleep(0.00)
 
-participants.sort()
-print(participants)
-
-input("press enter to make participants leave: ")
-for x in range(participantCount):
-    firstName = participants[x][0]
-    lastName = participants[x][1]
-    removeParticipant(firstName + " " + lastName, meetingName, meetingID, AdityaHostID,"generic@Email")
-    sleep(0.00)
-input("press enter to end the meeting: ")
+input("next")
 
 endMeeting(meetingName, meetingID, AdityaHostID)
